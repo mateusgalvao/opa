ae904bf340c7d3446a2d20fc66355385edde5808
mateusgalvao <mateusgalvao908@gmail.com>
2021-02-27 17:02:51 -0300

    Pro9

---
+++ Pro9.java
@@ -0,0 +1,28 @@
+public class Pro9{
+    public static void main(String args[]) {
+		int n = 1;
+		int nn = 1;
+		while (nn <= 100){
+			for(int i = 1; i <= 1 ; i++){
+				for(int j = 1; j <= i; j++){
+					if (n ==1){
+						System.out.print(n+" Java atrapalha muita gente ");
+						System.out.println();
+					
+					}else{
+						System.out.print(n+" Java atrapalham atrapalham muita gente");
+						System.out.println();
+	
+					}
+					n++;
+	
+				}
+	
+			}
+			
+		nn++;
+		}
+
+
+	}
+}
